c3573d3bb5ac237456c18193bdec2c397cc1d67f
Your Name <you@example.com>
2024-03-29 17:07:14 +0800

    03/29/2024 Fri

--- git-ac.bat
+++ git-ac.bat
@@ -1,5 +1,3 @@
-set date=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
-echo %data%
 
 @echo off
 Rem xcopy "C:\XUS\200-CODE\Dassault_Systemes\3DEXPERIENCE_XUS_Code\B423\win_b64"\*.* C:\XUS\100-APPS\3DEXPERIENCE_APP\CATApps\BMEDI_B423\CATApps\win_b64    /s /e /c /y /h /r  /d
